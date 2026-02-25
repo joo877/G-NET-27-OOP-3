2e9f6ca101209becaf7189c03ddfd537bdb9f3db
Youssef said <jooasaid2004@gmail.com>
2026-02-25 14:01:48 +0400

    Q02

--- OPP03/Program.cs
+++ OPP03/Program.cs
@@ -16,6 +16,24 @@
              
              */
             #endregion
+            #region Q02
+            /*
+             a- child class can access protected field from parnt. an object instance from chiled to outside can be access this field (behaivor as a private field)
+             
+              
+      b-                            with inheritance               Vs             without inheritance 
+
+     protected internal     access in same assembly and other                   access in same assembly only 
+    private protected         access in same assembly only                      access in class only   
+            
+
+
+            c- sealed with class :  class cannot inhirt (can be child , cannot be parent  and an still create objects  ) 
+               sealed with method :  prevent further overriding 
+
+            d- Yes! Sealed prevents inheritance, not instantiation. You can still create objects.
+             */
+            #endregion
             #endregion
         }
     }
