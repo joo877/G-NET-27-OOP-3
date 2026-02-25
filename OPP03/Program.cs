0b9cccd5cffaea4b053be37cd2bf009e34d2539b
Youssef said <jooasaid2004@gmail.com>
2026-02-25 13:45:37 +0400

    Q01

--- OPP03/Program.cs
+++ OPP03/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-          
+
+            #region Part01
+            #region Q1
+            /*
+             1-Composition
+             2-Association
+             3-Inheritance
+             4-Aggregation
+             5-Dependency
+             
+             */
+            #endregion
+            #endregion
         }
     }
 }
